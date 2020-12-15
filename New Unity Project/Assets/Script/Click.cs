fefc2c2b7afdc039e8411e65223c3ebba89b2512
Hugo Olaitan <Hugoolaitan@gmail.com>
2020-12-15 03:52:54 -0300

    Tiros e recarregar

---
+++ New Unity Project/Assets/Script/Click.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    public bool carregar = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        carregar = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+        RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+        if (hit.collider != null )
+        {
+            Debug.Log("hit.collider: " + hit.collider);
+            carregar = true;
+        }
+        else
+        {
+
+            Debug.Log("hit.collider: " + hit.collider);
+            carregar = false;
+        }
+
+    }
+
+    
+}
--- New Unity Project/Assets/Script/CursorScript.cs
+++ New Unity Project/Assets/Script/CursorScript.cs
@@ -8,12 +8,13 @@ public class CursorScript : MonoBehaviour
    
     public Text score;
     public int novoValor = 0;
+    public GameObject Mun;
+    private GameObject NovaBala;
 
     //munição
     public GameObject ItemBala;
-   
 
-    public GameObject[] ListaBalas;
+    private List<GameObject> ListaBalas;
 
     public Texture2D cursorTexture;
     public CursorMode cursorMode = CursorMode.Auto;
@@ -22,24 +23,29 @@ public class CursorScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        ListaBalas = new List<GameObject>();
+        Inicio();
     }
 
     // Update is called once per frame
     void Update()
     {
-        for(int i =0; i < 5; i++)
-        {
-            ListaBalas[i] = (GameObject)Instantiate(ItemBala, new Vector2(ItemBala.transform.position.x +i,
-                ItemBala.transform.position.y), Quaternion.identity);
-        }
-
-
+        
 
         if (Input.GetMouseButtonDown(0))
-        {
-            novoValor++;
-            score.text = novoValor.ToString();
+        {           
+            
+
+            if (Mun.GetComponent<Click>().carregar)
+            {
+                Repositorio();
+            }
+            else
+            {
+                novoValor++;
+                score.text = novoValor.ToString();
+                DestoiItem();
+            }
         }
 
         if (Input.GetMouseButtonDown(1))
@@ -53,9 +59,146 @@ public class CursorScript : MonoBehaviour
             Debug.Log("Pressed middle click.");
     }
 
+    GameObject criaMunicao(Vector2 posicao)
+    {
+        GameObject NovaBala;
+        NovaBala = Instantiate(ItemBala);
+        NovaBala.transform.position = posicao;
+        return NovaBala;
+    }
+
+    void Inicio()
+    {
+        float v = 0f;
+        for(int i=0; i<=5; i++)
+        {
+            NovaBala = criaMunicao(new Vector2(ItemBala.transform.position.x + v, ItemBala.transform.position.y));
+            
+            ListaBalas.Add(NovaBala);
+
+            switch (i) {
+                case 0:
+                    NovaBala.tag = "bala0";
+                break;
+                
+                case 1:
+                    NovaBala.tag = "bala1";
+                break;
+                
+                case 2:
+                    NovaBala.tag = "bala2";
+                break;
+                
+                case 3:
+                    NovaBala.tag = "bala3";
+                break;
+                
+                case 4:
+                    NovaBala.tag = "bala4";
+                break;
+                
+                case 5:
+                    NovaBala.tag = "bala5";
+                break; 
+            }            
+
+            v += 0.5f;
+        }
+    }
+
+    void DestoiItem()
+    {
+
+        
+        if(GameObject.FindWithTag("bala5") != null)
+        {
+            Destroy(GameObject.FindWithTag("bala5"));
+        }
+        else if (GameObject.FindWithTag("bala4") != null)
+        {
+            Destroy(GameObject.FindWithTag("bala4"));
+        }
+        else if (GameObject.FindWithTag("bala3") != null)
+        {
+            Destroy(GameObject.FindWithTag("bala3"));
+        }
+        else if (GameObject.FindWithTag("bala2") != null)
+        {
+            Destroy(GameObject.FindWithTag("bala2"));
+        }
+        else if (GameObject.FindWithTag("bala1") != null)
+        {
+            Destroy(GameObject.FindWithTag("bala1"));
+        }
+        else if (GameObject.FindWithTag("bala0") != null)
+        {
+            Destroy(GameObject.FindWithTag("bala0"));
+        }
+
+
+    }
+
+    void Repositorio()
+    {
+        Destroy(GameObject.FindWithTag("bala5"));
+        Destroy(GameObject.FindWithTag("bala4"));
+        Destroy(GameObject.FindWithTag("bala3"));
+        Destroy(GameObject.FindWithTag("bala2"));
+        Destroy(GameObject.FindWithTag("bala1"));
+        Destroy(GameObject.FindWithTag("bala0"));
+
+        float v = 0f;
+        
+        for (int i = 0; i <= 5; i++)
+        {
+            NovaBala = criaMunicao(new Vector2(ItemBala.transform.position.x + v, ItemBala.transform.position.y));
+
+            switch (i)
+            {
+                case 0:
+                    NovaBala.tag = "bala0";
+                    break;
+
+                case 1:
+                    NovaBala.tag = "bala1";
+                    break;
+
+                case 2:
+                    NovaBala.tag = "bala2";
+                    break;
+
+                case 3:
+                    NovaBala.tag = "bala3";
+                    break;
+
+                case 4:
+                    NovaBala.tag = "bala4";
+                    break;
+
+                case 5:
+                    NovaBala.tag = "bala5";
+                    break;
+                
+            }
+
+            v += 0.5f;
+
+        }
+    }
+
     void OnMouseEnter()
     {
-        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        //Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+
+    void OnMouseDown()
+    {
+        
+        if (Mun.tag == "municao")
+        {
+            Debug.Log("HIT");
+        }
+        Debug.Log("clicando na munição.");
     }
 }
 
